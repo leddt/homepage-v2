4bc5d9903567d10058fcd2e4e63ad4a3fe4bdbb7
David Thibault <thibault.david@gmail.com>
2017-01-20 22:05:39 -0500

    Use a jsonp package instead of handrolled implementation

--- app/yql.js
+++ app/yql.js
@@ -1,26 +1,11 @@
-if (!window.YQLCallbacks) {
-    window.YQLCallbacks = [];
-}
+import jsonp from "jsonp";
 
 function YQLQuery(query, callback) {
-    if (!query || !callback) {
-        throw new Error('YQLQuery: Parameters may be undefined');
-    }
-
-    var scriptEl = document.createElement('script'),
-        uid = 'yql' + +new Date(),
-        encodedQuery = encodeURIComponent(query.toLowerCase());
-
-    window.YQLCallbacks[uid] = function(json) {
-        callback(json);
-        delete window.YQLCallbacks[uid];
-        document.body.removeChild(scriptEl);
-    };
-
-    scriptEl.src = 'http://query.yahooapis.com/v1/public/yql?q='
-                 + encodedQuery + '&format=json&callback=YQLCallbacks.' + uid; 
-
-    document.body.appendChild(scriptEl);
+    const encodedQuery = encodeURIComponent(query.toLowerCase());
+    jsonp(`http://query.yahooapis.com/v1/public/yql?q=${encodedQuery}&format=json`, {}, (err, data) => {
+        if (!err)
+            callback(data)
+    });
 }
 
 export default YQLQuery;
