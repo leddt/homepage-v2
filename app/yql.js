7e6bb43e01b72bc45ae74c0378d1ca76e29727b0
David Thibault <thibault.david@gmail.com>
2018-08-01 07:55:26 -0400

    Use https for yql

--- app/yql.js
+++ app/yql.js
@@ -2,7 +2,7 @@ import jsonp from "jsonp";
 
 function YQLQuery(query, callback) {
     const encodedQuery = encodeURIComponent(query.toLowerCase());
-    jsonp(`http://query.yahooapis.com/v1/public/yql?q=${encodedQuery}&format=json`, {}, (err, data) => {
+    jsonp(`https://query.yahooapis.com/v1/public/yql?q=${encodedQuery}&format=json`, {}, (err, data) => {
         if (!err)
             callback(data)
     });
